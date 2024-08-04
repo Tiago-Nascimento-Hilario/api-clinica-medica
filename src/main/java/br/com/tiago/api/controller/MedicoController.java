c16e295f1145268016e2582fef52ce1738fd44bb
Tiago Nascimento Hilario <tnhhilario@gmail.com>
2024-08-04 14:22:35 -0300

    criado repository, classe e interface

--- src/main/java/br/com/tiago/api/controller/MedicoController.java
+++ src/main/java/br/com/tiago/api/controller/MedicoController.java
@@ -1,17 +1,25 @@
 package br.com.tiago.api.controller;
 
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.tiago.api.medico.CadastroMedico;
+import br.com.tiago.api.medico.DadosListagemMedico;
+import br.com.tiago.api.medico.Medico;
+import br.com.tiago.api.medico.MedicoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    private MedicoRepository repository;
     @PostMapping
-    public void cadastrar(@RequestBody String json){
-        System.out.println(json);
+    public void cadastrar(@RequestBody CadastroMedico dados){
+        repository.save(new Medico(dados));
+    }
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return  repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
---
+++ src/main/java/br/com/tiago/api/medico/Especialidade.java
@@ -0,0 +1,8 @@
+package br.com.tiago.api.medico;
+
+public enum Especialidade {
+    ORTOPEDIA,
+    CARDIOLOGIA,
+    GINECOLOGIA,
+    DERMATOLOGIA
+}
